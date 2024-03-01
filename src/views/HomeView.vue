902c38936d4de327ec46981e0e46343bf2c5bd4b
Monique-Silva <monique.silva-ximenes@le-campus-numerique.fr>
2024-03-01 16:46:25 +0100

    Add de logos et creation des dossiers

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <!--<TheWelcome />-->
+    <h1 class="mainPhrase">The easiest way to order your spirulina</h1>
+  </main>
+</template>
+
+<style>
+.mainPhrase {
+  font-style: oblique;
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .map {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
